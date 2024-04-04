2b5c4427ea24292bf3ca6dfc32896ae674d5cbab
robocontrol <kit.robocontrol@gmail.com>
2024-04-04 17:53:54 +0900

    fix:体裁, add:第3射出点

--- injection_interface/include/injection_interface/injection_interface_node.hpp
+++ injection_interface/include/injection_interface/injection_interface_node.hpp
@@ -33,11 +33,11 @@ public:
     explicit InjectionInterface(const std::string& name_space, const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
 private:
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr _sub_is_backside;
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr _sub_is_move_tracking;
-    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr _sub_self_pose;
-    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr _sub_move_target_pose;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _sub_move_node;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr _subscriber_is_backside;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr _subscriber_is_move_tracking;
+    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr _subscriber_self_pose;
+    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr _subscriber_move_target_pose;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _subscriber_move_node;
 
     void _callback_is_backside(const std_msgs::msg::Bool::SharedPtr msg);
     void _callback_is_move_tracking(const std_msgs::msg::Bool::SharedPtr msg);
@@ -45,28 +45,29 @@ private:
     void _callback_move_target_pose(const geometry_msgs::msg::Vector3::SharedPtr msg);
     void _callback_move_node(const std_msgs::msg::String::SharedPtr msg);
     
-    rclcpp::Publisher<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _pub_canusb;
-    rclcpp::Publisher<injection_interface_msg::msg::InjectionCommand>::SharedPtr _pub_injection;
-    rclcpp::Publisher<path_msg::msg::Turning>::SharedPtr _pub_spin_position;
+    rclcpp::Publisher<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _publisher_canusb;
+    rclcpp::Publisher<injection_interface_msg::msg::InjectionCommand>::SharedPtr _publisher_injection;
+    rclcpp::Publisher<path_msg::msg::Turning>::SharedPtr _publisher_spin_position;
 
     void set_calculate_vel(bool is_backside);
 
+    void command_calculation_vel();
     void command_injection_turn();
     void command_injection_pitch(double linear_pitch);
 
     rclcpp::QoS _qos = rclcpp::QoS(10);
 
-    const int16_t can_backspin_vel_id;
     const int16_t can_inject_pitch_id;
 
     TwoVector target_pos;
 
     //定数
     const std::vector<double> tf_injection2robot;
-    std::vector<double> strage_backside;
+    std::vector<double> strage_backside_0;
+    std::vector<double> strage_backside_1;
     std::vector<double> strage_front;
-    std::vector<double> linear_pitch;
-    std::vector<double> linear_tf;
+    const std::vector<double> linear_pitch;
+    const std::vector<double> linear_tf;
     std::vector<double> injection_gain;
 
     //フィールド
@@ -74,10 +75,12 @@ private:
     geometry_msgs::msg::Vector3 move_target_pose;
     bool is_move_tracking = false;
     bool is_correction_required = false;
-    bool last_target;
-    const std::string court_color_;
+    bool last_target = false;
     double pitch = 0.0;
     int injection_num  = 0;
+    std::string node = "";
+    TwoVector diff;
+    double target_height = 0.0;
 };
 
 }
--- injection_interface/src/injection_interface_node.cpp
+++ injection_interface/src/injection_interface_node.cpp
@@ -12,63 +12,58 @@ namespace injection_interface{
     InjectionInterface::InjectionInterface(const rclcpp::NodeOptions &options) : InjectionInterface("", options) {}
     InjectionInterface::InjectionInterface(const std::string &name_space, const rclcpp::NodeOptions &options)
         : rclcpp::Node("injection_interface_node", name_space, options),
-        
         tf_injection2robot(get_parameter("tf_injection2robot").as_double_array()),
-        strage_backside(get_parameter("strage_backside").as_double_array()),
+        strage_backside_0(get_parameter("strage_backside_0").as_double_array()),
+        strage_backside_1(get_parameter("strage_backside_1").as_double_array()),
         strage_front(get_parameter("strage_front").as_double_array()),
         linear_pitch(get_parameter("linear_pitch").as_double_array()),
         linear_tf(get_parameter("linear_tf").as_double_array()),
-        court_color_(get_parameter("court_color").as_string()),
         can_inject_pitch_id(get_parameter("canid.inject_pitch").as_int())
         
         {
-            _sub_is_backside = this->create_subscription<std_msgs::msg::Bool>(
+            _subscriber_is_backside = this->create_subscription<std_msgs::msg::Bool>(
                 "is_backside",
                 _qos,
                 std::bind(&InjectionInterface::_callback_is_backside, this, std::placeholders::_1)
             );
 
-            _sub_is_move_tracking = this->create_subscription<std_msgs::msg::Bool>(
+            _subscriber_is_move_tracking = this->create_subscription<std_msgs::msg::Bool>(
                 "is_move_tracking",
                 _qos,
                 std::bind(&InjectionInterface::_callback_is_move_tracking, this, std::placeholders::_1)
             );
 
-            _sub_self_pose = this->create_subscription<geometry_msgs::msg::Vector3>(
+            _subscriber_self_pose = this->create_subscription<geometry_msgs::msg::Vector3>(
                 "self_pose",
                 rclcpp::SensorDataQoS(),
                 std::bind(&InjectionInterface::_callback_self_pose, this, std::placeholders::_1)
             );
 
-            _sub_move_target_pose = this->create_subscription<geometry_msgs::msg::Vector3>(
+            _subscriber_move_target_pose = this->create_subscription<geometry_msgs::msg::Vector3>(
                 "move_target_pose",
                 _qos,
                 std::bind(&InjectionInterface::_callback_move_target_pose, this, std::placeholders::_1)
             );
 
-            _sub_move_node = this->create_subscription<std_msgs::msg::String>(
+            _subscriber_move_node = this->create_subscription<std_msgs::msg::String>(
                 "move_node",
                 _qos,
                 std::bind(&InjectionInterface::_callback_move_node, this, std::placeholders::_1)
             );
 
-            _pub_injection = this->create_publisher<injection_interface_msg::msg::InjectionCommand>("injection_command", 10);
-            _pub_spin_position = this->create_publisher<path_msg::msg::Turning>("spin_position", 10);
-            _pub_canusb = this->create_publisher<socketcan_interface_msg::msg::SocketcanIF>("can_tx", _qos);
+            _publisher_injection = this->create_publisher<injection_interface_msg::msg::InjectionCommand>("injection_command", 10);
+            _publisher_spin_position = this->create_publisher<path_msg::msg::Turning>("spin_position", 10);
+            _publisher_canusb = this->create_publisher<socketcan_interface_msg::msg::SocketcanIF>("can_tx", _qos);
 
+            const std::string court_color = this->get_parameter("court_color").as_string();
             const auto initial_pose = this->get_parameter("initial_pose").as_double_array();
-
-            if(court_color_ == "blue"){
-                self_pose.x = initial_pose[0];
-                self_pose.y = initial_pose[1];
-                self_pose.z = initial_pose[2];
-            }else if(court_color_ == "red"){
-                self_pose.x = initial_pose[0];
-                self_pose.y = -initial_pose[1];
-                self_pose.z = -initial_pose[2];
-                strage_front[1] *= -1;
-                strage_backside[1] *= -1;
-            }
+            const int coord_sign = (court_color == "blue") ? 1 : -1; 
+            self_pose.x = initial_pose[0];
+            self_pose.y = coord_sign * initial_pose[1];
+            self_pose.z = coord_sign * initial_pose[2];
+            strage_front[1] *= coord_sign;
+            strage_backside_0[1] *= coord_sign;
+            strage_backside_1[1] *= coord_sign;
 
             std::ifstream ifs(ament_index_cpp::get_package_share_directory("main_executor") + "/config/injection_interface/injection_gain.cfg");
             std::string str;
@@ -116,28 +111,53 @@ namespace injection_interface{
         }
 
         void InjectionInterface::set_calculate_vel(bool is_backside){
-            double target_height;
             bool target_input = false;
+            TwoVector injection_pos; //ロボットの本体座標と射出機構のずれを補正した数字
+            geometry_msgs::msg::Vector3 robot_pose;
             last_target = is_backside;
+
             if(is_backside){
                 cout<<"backside_vel"<<endl;
-                target_pos.x = strage_backside[0];
-                target_pos.y = strage_backside[1];
-                target_height = strage_backside[2];
+                switch (injection_num){
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    target_pos.x = strage_backside_0[0];
+                    target_pos.y = strage_backside_0[1];
+                    target_height = strage_backside_0[2];            
+                    break;
+                default:
+                    target_pos.x = strage_backside_1[0];
+                    target_pos.y = strage_backside_1[1];
+                    target_height = strage_backside_1[2];
+                    break;
+                }
+                injection_pos.x = robot_pose.x + linear_tf[0]*cos(robot_pose.z);
+                injection_pos.y = robot_pose.y + linear_tf[0]*sin(robot_pose.z);
+                command_injection_pitch(linear_pitch[0]);
                 target_input = true;
             }
-            else{
+            else {
                 cout<<"frontside_vel"<<endl;
                 target_pos.x = strage_front[0];
                 target_pos.y = strage_front[1];
                 target_height = strage_front[2];
+                injection_pos.x = robot_pose.x + linear_tf[1]*cos(robot_pose.z);
+                injection_pos.y = robot_pose.y + linear_tf[1]*sin(robot_pose.z); 
+                command_injection_pitch(linear_pitch[1]);
                 target_input = true;
             }
-            if(!target_input){
+            
+            if(!target_input) {
                 RCLCPP_INFO(this->get_logger(), "射出位置の入力ができませんでした");
                 return;
             }
-            geometry_msgs::msg::Vector3 robot_pose;
+
             if(is_move_tracking){
                 is_correction_required = true;
                 robot_pose = move_target_pose;
@@ -147,26 +167,18 @@ namespace injection_interface{
                 robot_pose = self_pose;
             }
 
-            //ロボットの本体座標と射出機構のずれを補正した数字
-            TwoVector injection_pos;
-            if(is_backside){
-                injection_pos.x = robot_pose.x + linear_tf[0]*cos(robot_pose.z);
-                injection_pos.y = robot_pose.y + linear_tf[0]*sin(robot_pose.z);
-                command_injection_pitch(linear_pitch[0]);
-            }
-            else{
-                injection_pos.x = robot_pose.x + linear_tf[1]*cos(robot_pose.z);
-                injection_pos.y = robot_pose.y + linear_tf[1]*sin(robot_pose.z); 
-                command_injection_pitch(linear_pitch[1]);
-            }
-            TwoVector diff = target_pos - injection_pos;
+            diff = target_pos - injection_pos;
+            command_calculation_vel();
+            command_injection_turn();    
+        }
+
+        void InjectionInterface::command_calculation_vel(){
             auto injection_command = std::make_shared<injection_interface_msg::msg::InjectionCommand>();
             injection_command->distance = diff.length();
             injection_command->height = target_height;
             injection_command->pitch = pitch;
             injection_command->gain = injection_gain[injection_num];
-            _pub_injection->publish(*injection_command);
-            command_injection_turn();    
+            _publisher_injection->publish(*injection_command);
         }
 
         void InjectionInterface::command_injection_turn(){
@@ -174,7 +186,7 @@ namespace injection_interface{
             auto injection_angle = std::make_shared<path_msg::msg::Turning>();
             injection_angle->angle_pos = atan2(target_pos.y - self_pose.y, target_pos.x - self_pose.x) - area(self_z, -f_pi, f_pi);
             injection_angle->accurate_convergence = true;
-            _pub_spin_position->publish(*injection_angle);
+            _publisher_spin_position->publish(*injection_angle);
         }
 
         void InjectionInterface::command_injection_pitch(double linear_pitch){
@@ -185,7 +197,7 @@ namespace injection_interface{
             msg_injection_pitch->canid = can_inject_pitch_id;
             msg_injection_pitch->candlc = 4;
             for(int i=0; i<msg_injection_pitch->candlc; i++) msg_injection_pitch->candata[i] = _candata[i];
-            _pub_canusb->publish(*msg_injection_pitch);
+            _publisher_canusb->publish(*msg_injection_pitch);
         }
 
 }  // namespace injection_interface
